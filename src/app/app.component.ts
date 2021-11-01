b5e1f816bbd10e6a65ea5efaa99490d519856aac
kirt registe <kirtregiste@outlook.com>
2021-11-01 18:55:40 -0500

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,43 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// export class AppComponent {
+//   viewMode = 'map';
+//   post = {
+//     title: "Title",
+//     isFavorite: true
+//   }
+//   tweet = {
+//     body: "Here is the body of a tweet..",
+//     isLiked: true,
+//     likesCount: 100
+
+//   }
+
+//   onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
+//     console.log("Favorite Changed: ", eventArgs);
+//   }
+//   onLikedChanged(eventArgs: any) {
+//     console.log("Liked changed: ", eventArgs)
+//   }
+// }
+// export class AppComponent {
+//   viewMode = 'list';
+// }
+
 export class AppComponent {
-  viewMode = 'map';
-  post = {
-    title: "Title",
-    isFavorite: true
+  courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+  ]
+  onAdd()
+  {
+    this.courses.push( {id: 4, name: 'course4'});
   }
-  tweet = {
-    body: "Here is the body of a tweet..",
-    isLiked: true,
-    likesCount: 100
-
-  }
-
-  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
-    console.log("Favorite Changed: ", eventArgs);
-  }
-  onLikedChanged(eventArgs: any) {
-    console.log("Liked changed: ", eventArgs)
+  onRemove(course: any)
+  {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
   }
 }
